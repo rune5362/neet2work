1410b7cf3e6800e0730f1721eee407d3c3789883
rune5362 <rune5362@gmail.com>
2026-06-02 17:39:21 +0900

    변경된 시간 표시 초단위로 변경

--- apps/frontend/src/pages/Documents.tsx
+++ apps/frontend/src/pages/Documents.tsx
@@ -64,7 +64,11 @@ function formatDate(value: string) {
   return new Intl.DateTimeFormat("ko-KR", {
     year: "numeric",
     month: "2-digit",
-    day: "2-digit"
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false
   }).format(new Date(value));
 }
 
